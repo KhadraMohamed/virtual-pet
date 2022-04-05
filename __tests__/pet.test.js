7bdfaa433a6896d8e55b6e42d12ef4e5d625b81d
khadra mohamed <khadramohamed@live.co.uk>
2022-04-05 16:44:01 +0100

    pet name

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,9 +1,12 @@
 const Pet = require('../src/pet');
 
-describe('constructor', () => {
-      it('sets the name property', () => {
-      const pet = new Pet('Fido');
-  
-      expect(pet.name).toEqual('Fido');
+describe ('constructor' , () => {
+    it ('returns an object' , () => {
+        expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
-  });
+    
+    it ('sets the name property' , () => {
+        const pet = new Pet ('Fido');
+        expect (pet.name).toEqual('Fido');
+     });
+});
