d4c00b1c2ccce80e3d58baba11f2c6ad4a6d49f3
khadra mohamed <khadramohamed@live.co.uk>
2022-04-06 21:59:42 +0100

    hunger and fitness

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -7,17 +7,37 @@ describe ('constructor' , () => {
     
     it ('sets the name property' , () => {
         const pet = new Pet ('Fido');
-        expect (pet.name).toEqual('Fido');
+        expect(pet.name).toEqual('Fido');
      });
      it ('has an initial age of 0' , () => {
          const pet = new Pet ('Fido');
-         expect (pet.age).toEqual(0);
+         expect(pet.age).toEqual(0);
      });
-});
-describe ('growUp' , () => {
+     it('has an initial hunger of 0' , () => {
+         const pet = new Pet ('Fido');
+         expect(pet.hunger).toEqual(0);
+     });
+     it('has an initial fitness of 10', () => {
+         const pet = new Pet ('Fido'); 
+         expect(pet.fitness).toEqual(10);
+     });
+
+     describe ('growUp' , () => {
     it ('increments the age by 1' , () => {
         const pet = new Pet ('Fido');
         pet.growUp();
         expect(pet.age).toEqual(1);
     });
+    it('increments the hunger by 5', () => {
+        const pet = new Pet  ('Fido');
+        pet.growUp();
+        expect(pet.hunger).toEqual(5);
+    });
+    it('reduces the fitness by 3', () => {
+        const pet = new Pet ('Fido');
+        pet.growUp();
+        expect(pet.fitness).toEqual(7);
+    });
+});
+
 });
